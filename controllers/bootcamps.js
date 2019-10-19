6282dd23a827183f2cbc7f88e056f36aa92152f5
RMH <rory.hennessey@gmail.com>
2019-10-19 09:34:10 -0400

    routes started, models started, controllers started, dummy data brought in

---
+++ controllers/bootcamps.js
@@ -0,0 +1,57 @@
+const Bootcamp = require('../models/Bootcamp');
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Show all bootcamps'
+  });
+};
+
+// @desc    Get single bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Show bootcamp ${req.params.id}`
+  });
+};
+
+// @desc    Create single bootcamp
+// @route   POST /api/v1/bootcamps/
+// @access  Private
+
+exports.createBootcamp = (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({
+    success: true,
+    msg: 'Create new bootcamp'
+  });
+};
+
+// @desc    Update single bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update bootcamp ${req.params.id}`
+  });
+};
+
+// @desc    Delete single bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootcamp ${req.params.id}`
+  });
+};
