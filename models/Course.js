80d0255737a101fd0f8af2e71e977b894c42186d
RMH <rory.hennessey@gmail.com>
2019-10-20 10:49:07 -0400

    Course model created, seeder updated

---
+++ models/Course.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a course title']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description']
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please add number of weeks']
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please add a tuition cost']
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add a minimum skill'],
+    enum: ['beginner', 'intermediate', 'advanced']
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  bootcamp: {
+    // need to add this so we know what course goes with what bootcamp
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp', // need so we know what model to reference
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
