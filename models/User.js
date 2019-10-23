98e836481b55c838098a000483a6585aa3430560
RMH <rory.hennessey@gmail.com>
2019-10-23 09:31:49 -0400

    forgot password functionality & route implemented

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
@@ -39,6 +40,10 @@ const UserSchema = new mongoose.Schema({
 // Encrypt password using bcrypt
 
 UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt); // we have access to pw from auth.js in controllers
 });
@@ -55,5 +60,22 @@ UserSchema.methods.getSignedJwtToken = function () {
 UserSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 };
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+    // Generate Token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash token and set to resetPasswordToken field -- this is all found in the node documentation
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+}
 
 module.exports = mongoose.model('User', UserSchema);
