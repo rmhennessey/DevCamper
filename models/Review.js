f91056a60f56bc25f9890f01ff261c402ef09a0f
RMH <rory.hennessey@gmail.com>
2019-10-24 12:27:56 -0400

    update and delete review functionality added

--- models/Review.js
+++ models/Review.js
@@ -38,4 +38,41 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true })
 
+// Static method to get avg rating and save - Static means it have to run on the model (see Middleware below)
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+    // console.log('Calculating avg cost...'.blue)
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+
+    // console.log(obj);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Middleware Call getAverageRating after save 
+ReviewSchema.post('save', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+})
+
+// Middleware Call getAverageRating before remove
+ReviewSchema.pre('remove', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+})
+
+
 module.exports = mongoose.model('Review', ReviewSchema);
